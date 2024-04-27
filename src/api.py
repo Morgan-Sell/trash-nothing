2d998065e26dcede9d3f2b9ea9ec7e189a8c8ae1
Morgan-Sell <morganpsell@gmail.com>
2024-04-27 14:16:01 -0400

    wrote docstring for fetch_data()

--- src/api.py
+++ src/api.py
@@ -9,19 +9,24 @@ from .config import JSONType
 
 async def fetch_data(endpoint: str, params: dict, page: int) -> JSONType:
     """
-    Sends a GET request to a specified URL endpoint with optional parameters and headers,
-    returns the response data as a JSON object.
+    Asynchronously fetches data from a specified endpoint with given parameters.
+
+    This function initializes an asynchronous HTTP session and sends a GET request to
+    the provided endpoint. It includes support for pagination through the 'page' parameter.
+    It returns the JSON response if the HTTP status code is 200.
 
     Parameters:
-    - endpoint (str): The URL to which the GET request is sent.
-    - params (dict): Parameters to be sent with the request.
-    - headers (dict, optional): HTTP headers to send with the request. Default is an empty dictionary.
+    - endpoint (str): The URL of the API endpoint to send the request to.
+    - params (dict): A dictionary of parameters to be passed with the request.
+    - page (int): The page number to be requested, used for pagination.
 
     Returns:
-    - JSONType: A JSON object containing the response data.
+    - JSONType: A JSON object containing the response data if the request is successful.
 
     Raises:
-    - HTTPError: If the response status is not 200, indicating the fetch was unsuccessful.
+    - ClientResponseError: If the response status is not 200, the function will
+      raise an exception with the response's error status and message.
+
     """
     params["page"] = page
     async with aiohttp.ClientSession() as session:
