58e1310aeef0fe666343d87f76c2bc1cc057097b
Morgan-Sell <morganpsell@gmail.com>
2024-04-27 10:18:10 -0400

    created create_countplot()

--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,14 @@
 import plotly.express as px
+from plotly.graph_objs._figure import Figure
 import pandas as pd
 
 
-def create_histogram(data: pd.Series, title: str):
+def create_histogram(data: pd.Series, title: str) -> Figure:
     return px.histogram(data, title=title)
+
+
+def create_countplot(data: pd.DataFrame, variable: str, title: str) -> Figure:
+    fig = px.histogram(data, x=variable, title=title, text_auto=True)
+    fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+    fig.update_layout(bargap=0.2)
+    return fig
