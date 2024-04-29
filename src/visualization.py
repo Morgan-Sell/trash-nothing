6c0316917d6012116679ab89a5ffea979ef91870
Morgan-Sell <morganpsell@gmail.com>
2024-04-29 15:54:46 -0700

    refactored src/visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,11 +1,14 @@
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure as plt_figure
 import plotly.express as px
-from plotly.graph_objs._figure import Figure
+from plotly.graph_objs._figure import Figure as px_figure
 import pandas as pd
+from wordcloud import WordCloud, STOPWORDS
 
 
 def create_histogram(
     data: pd.DataFrame, variable: str, title: str, xaxis: str, yaxis: str
-) -> Figure:
+) -> px_figure:
     fig = px.histogram(data, x=variable)
 
     # format figure
@@ -28,7 +31,7 @@ def create_histogram(
 
 def create_countplot(
     data: pd.DataFrame, variable: str, title: str, xaxis: str, yaxis: str
-) -> Figure:
+) -> px_figure:
     fig = px.histogram(data, x=variable, text_auto=True)
 
     # format figure
@@ -50,4 +53,18 @@ def create_countplot(
         textfont_size=12, textangle=0, textposition="outside", cliponaxis=False
     )
 
+    return fig
+
+
+def create_word_cloud(text: str) -> plt_figure:
+
+    wordcloud = WordCloud(
+        width=800, height=300, stopwords=STOPWORDS, background_color="white"
+    ).generate(text)
+
+    # create word cloud with matplotlib
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.imshow(wordcloud, interpolation="bilinear")
+    ax.axis("off")
+    
     return fig
